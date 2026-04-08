d9118eb52391b9c905d7f0361d1761cd3a8bc466
ant <jack2001061224680@gmail.com>
2026-04-08 10:37:49 +0800

    4/8-update1

---
+++ myapp/views.py
@@ -0,0 +1,158 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from myapp.models import *
+from django.forms.models import model_to_dict
+from django.shortcuts import redirect
+from django.core.paginator import Paginator
+
+
+# Create your views here.
+
+def search_list(request):
+    if 'cname' in request.GET:
+        cname = request.GET['cname']
+        print(cname)
+        result_list = students.objects.filter(cname__contains=cname)
+    else:
+        result_list = students.objects.all().order_by('cid')
+
+
+    for item in result_list:
+        print(model_to_dict(item))
+    # return HttpResponse("hiiii")
+    # result_list = [] #模擬沒有資料的情況下 ，讓errormessage有值
+    errormessage = ""
+    if not result_list:
+        errormessage = "No data found."
+    # return render(request, 'search_list.html', locals())
+    return render(request, 'search_list.html', {'result_list': result_list, 'errormessage': errormessage})
+
+
+def search_name(request):
+    return render(request, 'search_name.html') #沒有變數要拋 所以沒有locals不影響
+
+# def index(request):
+#     result_list = students.objects.all().order_by('cid')
+#     for item in result_list:
+#         print(model_to_dict(item))
+#         if item.csex == 'M':
+#             item.csex = '男'
+#         else:
+#             item.csex = '女'
+#     datacount = len(result_list)
+#     print(f"Total data count:{datacount}")
+#     status = True
+#     errormessage=""
+#     if not result_list:
+#         status = False
+#         errormessage = "No data found."
+#     # return HttpResponse("Hii")
+#     return render(request, 'index.html',
+#                    {'result_list': result_list, 'status': status, 'errormessage': errormessage , 'datacount':datacount})
+
+def post(request):
+    if request.method == "POST":
+        cname = request.POST.get('cname')
+        csex  = request.POST.get('csex')
+        cbirthday = request.POST.get('cbirthday')
+        cemail = request.POST.get('cemail')
+        cphone = request.POST.get('cphone')
+        caddr  = request.POST.get('caddr')
+        print(f"Recevied Post data : cname:{cname},csex:{csex},cbirthday:{cbirthday},cemail:{cemail},cphone:{cphone},caddr:{caddr}")
+        add = students(cname = cname, csex = csex,cbirthday = cbirthday,cemail = cemail,cphone=cphone,caddr=caddr)
+        add.save()
+        # return HttpResponse("已送出 POST 請求")
+        return redirect('index')
+    else:
+        return render (request,'post.html')
+    
+
+
+def edit(request,id):
+    print(id)
+    if request.method == 'POST':
+        cname = request.POST.get('cname')
+        csex  = request.POST.get('csex')
+        cbirthday = request.POST.get('cbirthday')
+        cemail = request.POST.get('cemail')
+        cphone = request.POST.get('cphone')
+        caddr  = request.POST.get('caddr')
+        print(f"Recevied Post data : cname:{cname},csex:{csex},cbirthday:{cbirthday},cemail:{cemail},cphone:{cphone},caddr:{caddr}")
+        update = students.objects.get(cid=id)
+        update.cname = cname
+        update.csex = csex
+        update.cbirthday = cbirthday
+        update.cemail = cemail
+        update.cphone = cphone
+        update.caddr = caddr
+        update.save()
+        return redirect('index')
+        # return HttpResponse("已送出POST請求")
+    else:
+        obj_data = students.objects.get(cid=id)
+        print(model_to_dict(obj_data))
+        # return HttpResponse("Hiiii")
+        return render (request,'edit.html',{'obj_data':obj_data})
+    
+def delete(request,id):
+    print(id)
+    if request.method == 'POST':
+        delete_data = students.objects.get(cid=id)
+        delete_data.delete()
+        return redirect('index')
+    else:
+        obj_data = students.objects.get(cid=id)
+        print(model_to_dict(obj_data))
+        return render(request,'delete.html',{'obj_data':obj_data})
+    
+from django.db.models import Q
+
+
+def index(request):
+    site_search = request.GET.get("site_search", "").strip()
+
+    if site_search:
+        keywords = site_search.split()
+        print(keywords)
+
+        query = Q()
+
+        for keyword in keywords:
+            keyword_query = (
+                Q(cid__icontains=keyword) |
+                Q(cname__icontains=keyword) |
+                Q(cbirthday__icontains=keyword) |
+                Q(cemail__icontains=keyword) |
+                Q(cphone__icontains=keyword) |
+                Q(caddr__icontains=keyword)
+            )
+            query |= keyword_query 
+
+        resultList = students.objects.filter(query).order_by('cid')
+
+    else:
+        resultList = students.objects.all().order_by('cid')
+
+    for item in resultList:
+        print(model_to_dict(item))
+
+    data_count = resultList.count()
+    print(f"Total data count: {data_count}")
+
+    status = resultList.exists()
+    errormessage = "" if status else "No data found"
+    #分頁設定 每頁顯示2筆資料
+    paginator = Paginator(resultList, 2)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    print(page_number)
+    for item in page_obj:
+        print(model_to_dict(item))
+
+    return render(request, 'index.html', {
+        'resultList': resultList,
+        'data_count': data_count,
+        'status': status,
+        'errormessage': errormessage,
+        'page_obj': page_obj
+    })
